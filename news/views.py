8a22a68ce88d2cfea222154e6c7a133768a131d0
Rostyslav Kopotiienko <rost.kop.200.5@gmail.com>
2025-09-15 18:21:40 +0200

    Add urls of home page, make view and template for home page

--- news/views.py
+++ news/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from news.models import Topic, Redactor, Newspaper
 
-# Create your views here.
+
+def index(request):
+    number_of_topics = Topic.objects.count()
+    number_of_redactors = Redactor.objects.count()
+    number_of_newspapers = Newspaper.objects.count()
+    last_published_newspaper = Newspaper.objects.order_by("-published_date").first()
+
+    context = {
+        "number_of_topics": number_of_topics,
+        "number_of_redactors": number_of_redactors,
+        "number_of_newspapers": number_of_newspapers,
+        "last_newspaper": last_published_newspaper
+    }
+
+    return render(request, "news/index.html", context=context)
