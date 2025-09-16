f747b9aca320f1d590e446e23c591b8ce198abe7
Rostyslav Kopotiienko <rost.kop.200.5@gmail.com>
2025-09-16 22:36:30 +0200

    Add list pages for Newspaper, Redactor, Topic; add sidebar nav

--- news/urls.py
+++ news/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from .views import (
-    index,
+    index, TopicListView, NewspaperListView, RedactorListView,
 )
 
 
 urlpatterns = [
     path("", index, name="index"),
+    path("topics/", TopicListView.as_view(), name="topic-list"),
+    path("newspapers/", NewspaperListView.as_view(), name="newspaper-list"),
+    path("redactors/", RedactorListView.as_view(), name="redactor-list")
 ]
 
 app_name = "news"
--- news/views.py
+++ news/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
+
 from news.models import Topic, Redactor, Newspaper
 
 
@@ -16,3 +19,22 @@ def index(request):
     }
 
     return render(request, "news/index.html", context=context)
+
+
+class TopicListView(LoginRequiredMixin, generic.ListView):
+    model = Topic
+    paginate_by = 5
+    template_name = "news/topic_list.html"
+
+
+class NewspaperListView(LoginRequiredMixin, generic.ListView):
+    model = Newspaper
+    paginate_by = 5
+    template_name = "news/newspaper_list.html"
+
+
+
+class RedactorListView(LoginRequiredMixin, generic.ListView):
+    model = Redactor
+    paginate_by = 5
+    template_name = "news/redactor_list.html"
