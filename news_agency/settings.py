67f188375f02e857f1ad7c52610cb3d7bb594ace
Rostyslav Kopotiienko <rost.kop.200.5@gmail.com>
2025-10-03 22:40:24 +0200

    Move SECRET_KEY from settings.py to environment variables (.env)

--- news_agency/settings.py
+++ news_agency/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nej)o(=jkhc%t^$!+-z@q&+w&h=y(0lq)t_r9@51lc)7317r+='
+from decouple import config
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
